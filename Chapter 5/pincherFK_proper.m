eb890df74c9c6478defeffea250203f7880de594
NabihaHasnain09355 <156357524+Nabiha09355@users.noreply.github.com>
2026-04-22 23:20:48 +0500

    Update pincherFK_proper.m

--- Chapter 5/pincherFK_proper.m
+++ Chapter 5/pincherFK_proper.m
@@ -2,7 +2,7 @@ function [x, y, z, R] = pincherFK_proper(jointAngles)
     % pincherFK: Calculates Forward Kinematics for Phantom X Pincher 
     %
     % Inputs:
-    %   jointAngles - 1x4 vector of DH joint angles [th1, th2, th3, th4] in radians [cite: 1073, 1076]
+    %   jointAngles - 1x4 vector of DH joint angles [th1, th2, th3, th4] in radians 
     %
     % Outputs:
     %   x, y, z - Cartesian position of the gripper in mm 
@@ -21,7 +21,7 @@ function [x, y, z, R] = pincherFK_proper(jointAngles)
     th4 = jointAngles(4);
 
     % --- 3. DH Transformation Helper ---
-    % Standard DH formula [cite: 1913]
+    % Standard DH formula 
     dh = @(theta, d, a, alpha) [
         cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha), a*cos(theta);
         sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
@@ -30,19 +30,18 @@ function [x, y, z, R] = pincherFK_proper(jointAngles)
     ];
 
     % --- 4. Link Transformations ---
-    % Using alpha values from your assignment: [90, 0, 0, 0]
     T01 = dh(th1, d1, 0, pi/2);
     T12 = dh(th2, 0, a2, 0);
     T23 = dh(th3, 0, a3, 0);
     T34 = dh(th4, 0, a4, 0);
 
     % --- 5. Full Transformation Matrix ---
-    % Multiplied in order from base to end-effector [cite: 1057]
+    % Multiplied in order from base to end-effector 
     T04 = T01 * T12 * T23 * T34;
 
     % --- 6. Extract Outputs ---
     x = T04(1,4);
     y = T04(2,4);
     z = T04(3,4);
-    R = T04(1:3, 1:3); % Rotation matrix is the top-left 3x3 sub-matrix [cite: 1066]
+    R = T04(1:3, 1:3); % Rotation matrix is the top-left 3x3 sub-matrix 
 end
